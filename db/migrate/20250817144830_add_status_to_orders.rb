5025a9d923d37193a0d9145005d03a39e2e6acac
VitorMiguelRosa <vitorvlogg3rmito@gmail.com>
2025-08-17 12:35:13 -0300

    another hotfix

--- db/migrate/20250817144830_add_status_to_orders.rb
+++ db/migrate/20250817144830_add_status_to_orders.rb
@@ -1,6 +1,8 @@
-class AddStripeSessionIdToOrders < ActiveRecord::Migration[7.1]
+class AddStatusToOrders < ActiveRecord::Migration[7.1]
   def change
     add_column :orders, :stripe_session_id, :string
     add_index :orders, :stripe_session_id, unique: true
+    add_column :orders, :status, :integer, default: 0
+    add_index :orders, :status
   end
 end
